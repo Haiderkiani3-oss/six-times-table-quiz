af2ab074341bf9edf8d5df6fb239fa192682d923
Haiderkiani3-oss <muqaddaskiani3@gmail.com>
2026-06-03 22:07:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Label('Master Your 6 Times Table!',200,35,size=28,bold=True,fill='coral')
 app.allAnswers=[6,12,18,24,30,36,42,48,54,60]
 # User's question Number 
 app.questionNumber= 0
-app.wrongClicks=0
+app.wrongClicks= 0
 app.totalRight= 0
 # Whether the User Passed or Failed
 app.scoreHistory= []
